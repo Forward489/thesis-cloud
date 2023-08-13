591f5a952e337c4733e237bb22d9c241d2ed79b1
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-13 15:09:17 +0700

    refactor: refactoring script and deleting books

--- bdl.sh
+++ bdl.sh
@@ -1,3 +1,3 @@
-sh install-command-runner.sh
+# sh ./install-command-runner.sh
 
 aws cloudformation create-stack --stack-name "BDL Instance" --template-body ./yamls/bdl.yaml
--- cloud9.sh
+++ cloud9.sh
@@ -1,3 +1,12 @@
-sh install-command-runner.sh
+# sh install-command-runner.sh
 
 aws cloudformation create-stack --stack-name "Tekweb IDE" --template-body ./yamls/cloud-9.yaml
+# instance=`aws ec2 describe-instances --filters 'Name=tag:Name,Values=*cloud9*' --output text --query 'Reservations[*].Instances[*].InstanceId'`
+
+security=`aws ec2 describe-instances --filters 'Name=tag:Name,Values=*cloud9*' --output text --query 'Reservations[*].Instances[*].[InstanceId,NetworkInterfaces[*].Groups[*].GroupId,NetworkInterfaces[*].SubnetId][0][1]'`
+
+aws ec2 authorize-security-group-ingress \
+--group-id $security \
+--protocol tcp \
+--port 80 \
+--cidr 0.0.0.0/0
