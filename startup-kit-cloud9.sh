e807bd1b37d87307249c9624c8a594af8f2ff50e
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-13 15:39:07 +0700

    refactor: changing cli command in startup kit script

--- startup-kit-cloud9.sh
+++ startup-kit-cloud9.sh
@@ -1,4 +1,4 @@
-#pls run "git clone https://github.com/Forward489/thesis-cloud.git && cd thesis-cloud && sh startup-kit-cloud9.sh" in cloud9 terminal
+#pls run "git clone https://github.com/Forward489/thesis-cloud.git && cd thesis-cloud && sh startup-kit-cloud9.sh && cd .. && rm -r thesis-cloud" in cloud9 terminal
 
 sudo chown ubuntu:ubuntu /var/www/html
 sudo ln -s /var/www/html/ ~/environment/public-html
