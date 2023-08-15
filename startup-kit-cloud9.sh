db18223e35b440c38281c7033f982aba791d9201
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-15 12:10:05 +0700

    fix: fixing structure for Bab 2

--- startup-kit-cloud9.sh
+++ startup-kit-cloud9.sh
@@ -1,4 +1,6 @@
-#pls run "git clone https://github.com/Forward489/thesis-cloud.git && cd thesis-cloud && sh startup-kit-cloud9.sh && cd .. && sudo rm -r thesis-cloud" in cloud9 terminal
+#pls run "git clone https://github.com/Forward489/thesis-cloud.git && cd thesis-cloud && sh startup-kit-cloud9.sh"
 
 sudo chown ubuntu:ubuntu /var/www/html
 sudo ln -s /var/www/html/ ~/environment/public-html
+cd ..
+sudo rm -r thesis-cloud" in cloud9 terminal
