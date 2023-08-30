a7ebc9a62c48bcf60e59fe99e05d995bef73eeb9
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-08-30 12:02:46 +0700

    feat: add config for security groups

--- bdl.sh
+++ bdl.sh
@@ -4,7 +4,13 @@ aws cloudformation deploy \
 --template-file ./yamls/bdl.yaml \
 --stack-name BDL
 
+security=`aws ec2 describe-instances --filters 'Name=tag:Name,Values=*BDL*' --output text --query 'Reservations[*].Instances[*].[InstanceId,NetworkInterfaces[*].Groups[*].GroupId,NetworkInterfaces[*].SubnetId][0][1]'`
+
+aws ec2 authorize-security-group-ingress \
+--group-id $security \
+--protocol tcp \
+--port 3389 \
+--cidr 0.0.0.0/0
+
 cd ..
 sudo rm -r thesis-cloud
-
-# security=`aws ec2 describe-instances --filters 'Name=tag:Name,Values=*-*' --output text --query 'Reservations[*].Instances[*].[InstanceId,NetworkInterfaces[*].Groups[*].GroupId,NetworkInterfaces[*].SubnetId][0][1]'`
