f1baf3f6fa21bcf561d46c8c2d11e254fb4eb99e
Glenn Steven Santoso <glennstevencg21@gmail.com>
2023-09-03 17:06:51 +0700

    fix: fix cloud9 yaml script 16

--- startup-kit-cloud9.sh
+++ startup-kit-cloud9.sh
@@ -1,7 +1,5 @@
 #please run "git clone https://github.com/Forward489/thesis-cloud.git && cd thesis-cloud && sh startup-kit-cloud9.sh"
 
-touch /var/www/html/tes.php
-echo "<?php \n\tphpinfo(); \n?>" >> /var/www/html/tes.php
 sudo chown ubuntu:ubuntu /var/www/html
 sudo ln -s /var/www/html/ ~/environment/public-html
 cd ..
